735b0e88e3267ad74382e296a3d49b4424b67111
runningman-612 <runningman@riseup.net>
2019-07-05 22:40:49 +0800

    Disable PM2 Logging

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,17 @@
 module.exports = {
   apps: [
     {
-      name: 'bigo',
-      script: 'bigo.js',
+      name: "bigo",
+      script: "bigo.js",
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
-      exec_mode: 'cluster',
-      ignore_watch: ['node_modules'],
+      exec_mode: "cluster",
+      ignore_watch: ["node_modules"],
       autorestart: true,
-      watch: ['forcereboot.js'],
-      max_memory_restart: '1G',
-    },
-  ],
+      watch: ["forcereboot.js"],
+      max_memory_restart: "1G",
+      error_file: "NULL",
+      out_file: "NULL"
+    }
+  ]
 };
